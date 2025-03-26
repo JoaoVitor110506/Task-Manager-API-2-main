ef4b4d80bb03a9ace7fa7822e9bb575da6278cc2
João Vitor <joaovitorsouza110506@gmail.com>
2025-03-26 20:57:35 -0300

    add base task services and paginations

---
+++ src/repositories/taskRepository.ts
@@ -0,0 +1,78 @@
+import { sqliteConnection } from "../databases";
+import { TaskDataCreate } from "../services/taskServices";
+
+
+export type CreateTaskDataType = TaskDataCreate & { id: string };
+export type UpdateTaskDataType = CreateTaskDataType & { update_at: Date };
+
+export const taskRepository = {
+  async createTask({ id, title, description, date, status, user_id }: CreateTaskDataType) {
+    try {
+      const db = await sqliteConnection();
+
+      const query = `
+      INSERT INTO tasks (id, title, description, date, status, user_id)
+      VALUES (?, ?, ?, ?, ?, ?)
+    `;
+
+      await db.run(query, [id, title, description, date, status, user_id]);
+
+      return { id, title, description, date, status, user_id };
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async getTaskByID(id: string) {
+    try {
+      const db = await sqliteConnection();
+
+      const query = `SELECT * FROM tasks WHERE id = ?`;
+
+      const task = await db.get(query, id);
+
+      return task;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async updateTask({
+    id,
+    title,
+    description,
+    date,
+    status,
+    user_id,
+    update_at,
+  }: UpdateTaskDataType) {
+    try {
+      const db = await sqliteConnection();
+
+      const query = `
+      UPDATE tasks
+      SET title = ?, description = ?, date = ?, status = ?, update_at = ?
+      WHERE id = ? AND user_id = ?
+    `;
+
+      await db.run(query, [title, description, date, status, update_at, id, user_id]);
+
+      return { id, title, description, date, status, user_id, update_at };
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async deleteTaskByID(id: string) {
+    try {
+      const db = await sqliteConnection();
+
+      const query = `DELETE FROM tasks WHERE id = ?`;
+      await db.run(query, id);
+
+      return { id };
+    } catch (error) {
+      throw error;
+    }
+  },
+};
